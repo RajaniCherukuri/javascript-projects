a77ff3f2060a00d307361378cd214ed0f2a9eebc
RajaniCherukuri <rajanic7979@gmail.com>
2024-02-08 21:58:33 -0600

    Completed

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,16 +1,16 @@
-const checkFive = require("../checkFive.js")
+const test = require("../checkFive.js")
 
 describe("checkFive solution",function(){
-    test("Returns message-'number is less than 5',when the number entered is less than 5",function(){
-       let output = checkFive.checkFive(2);
+    it("Returns message-'number is less than 5',when the number entered is less than 5",function(){
+       let output = test.checkFive(2);
         expect(output).toEqual("2 is less than 5.")
     })
-    test("Returns message-'number is greater than 5',when the number entered is less than 20",function(){
-        let output = checkFive.checkFive(20);
+    it("Returns message-'number is greater than 5',when the number entered is less than 20",function(){
+        let output = test.checkFive(20);
          expect(output).toEqual("20 is greater than 5.")
      })
-    test("Returns message-'number is equal to 5',when the number entered is 5",function(){
-        let output = checkFive.checkFive(5);
+     it("Returns message-'number is equal to 5',when the number entered is 5",function(){
+        let output = test.checkFive(5);
          expect(output).toEqual("5 is equal to 5.")
      })
 });
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,25 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const test = require('../index.js');
 
 describe("Testing launchcode", function(){
-
   // Write your unit tests here!
-  
+  it('Returns value as "nonprofit" for the key organization',function(){
+    expect(test.launchcode.organization).toBe("nonprofit")
+  })
+  it('Returns value as "Jeff" for the key executiveDirector',function(){
+    expect(test.launchcode.executiveDirector).toBe("Jeff")
+  })
+  it('Returns value as 100 for the key percentageCoolEmployees',function(){
+    expect(test.launchcode.percentageCoolEmployees).toBe(100)
+  })
+  it('Returns value as "Web Development", "Data Analysis" and "Liftoff" for the key programsOffered',function(){
+    expect(test.launchcode.programsOffered[0]).toBe("Web Development")
+    expect(test.launchcode.programsOffered[1]).toBe("Data Analysis")
+    expect(test.launchcode.programsOffered[2]).toBe("Liftoff")
+  })
+  it('Returns length for the key programsOffered',function(){
+    expect(test.launchcode.programsOffered.length).toBe(3)
+  })
+
+ 
 });
