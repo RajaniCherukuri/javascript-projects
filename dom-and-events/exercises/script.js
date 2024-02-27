5b7ea579171223364075696c64457072bb7071dd
RajaniCherukuri <rajanic7979@gmail.com>
2024-02-26 20:14:19 -0600

    Completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,29 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "Red";
+     });
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+     let button = document.getElementById("abortMission");
+     button.addEventListener("click", function (event) {
+         console.log("Are you sure you want to abort the mission?");
+        });
+          
+     
     
 }
 
 window.addEventListener("load", init);
+
+
+// function changeColor(newColor) {
+//     const elem = document.getElementById("para");
+//     elem.style.color = newColor;
+//   }
+  
--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,14 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+const missionAbort = document.getElementById("missionAbort");
+const upButton = document.getElementById("up");
+const downButton = document.getElementById("down");
+const rightButton = document.getElementById("right");
+const leftButton = document.getElementById("left");
+const flightStatus = document.getElementById("flightStatus");
+const endlessStatus = document.getElementById("shuttleBackground");
+const shuttleHeight = document.getElementById("spaceShuttleHeight");
+
+
+
